bfe54e59bdba93e58466ec23476367d01563fcc8
Suzi216 <suzana.marsela@gmail.com>
2023-01-31 21:37:36 +0300

    Adding he teacher classes

--- person.rb
+++ person.rb
@@ -1,21 +1,23 @@
 class Person
-  def initialize(name = 'Unknown', age, parent_permission:true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
-    @age=age
+    @age = age
     @parent_permission = parent_permission
   end
-  def id
-    @id
-  end
+
+  attr_reader :id
   attr_accessor :name, :age
 
   private
-  def is_of_age?
-    @age>=18
+
+  def of_age?
+    return true if @age >= 18
+
+    false
   end
+
   def can_use_services?
     is_of_age && parent_permission
   end
 end
-
--- student.rb
+++ student.rb
@@ -1,12 +1,11 @@
 require './Person'
 class Student < Person
-  def initialize(name = 'Unknown', age, parent_permission:true, classroom)
+  def initialize(age, classroom, name: 'unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
   end
 
-  def play_hooky 
-    '"¯\(ツ)/¯"'
+  def play_hooky
+    '¯\(ツ)/¯'
   end
 end
-
