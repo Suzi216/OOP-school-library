7c960e5de8bdc717ef6cb5aa4638602a74d039e1
Suzi216 <suzana.marsela@gmail.com>
2023-02-02 18:56:43 +0300

    Fixing linters

--- rental.rb
+++ rental.rb
@@ -1,11 +1,10 @@
 class Rental
-    def initialize(person, book, date)
-        @person=person
-        @book=book
-        @date=date
-        @person.rentals.push(self) unless person.rentals.include?(self)
-        @book.rentals.push(self) unless book.rentals.include?(self)
-    end
-    attr_accessor :date, :book, :person
-
+  def initialize(person, book, date)
+    @person = person
+    @book = book
+    @date = date
+    @person.rentals.push(self) unless person.rentals.include?(self)
+    @book.rentals.push(self) unless book.rentals.include?(self)
+  end
+  attr_accessor :date, :book, :person
 end
